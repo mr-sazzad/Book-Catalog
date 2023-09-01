4eaaa3340ae816018e21db9134731ee18cd2c8ed
mr-sazzad <main.sazza@gmail.com>
2023-09-01 23:54:02 +0600

    get all categories commited

--- src/app/moduls/categories/category_controller.ts
+++ src/app/moduls/categories/category_controller.ts
@@ -18,3 +18,19 @@ export const createCategory: RequestHandler = async (req, res, next) => {
     next(err);
   }
 };
+
+export const getAllCategories: RequestHandler = async (req, res, next) => {
+  try {
+    const result = await categoryServices.getAllCategories();
+
+    res.status(200).json({
+      statusCode: 200,
+      success: true,
+      message: "Categories retrieved Successfully 🦀",
+      data: result,
+    });
+  } catch (err: any) {
+    console.log(err);
+    next(err);
+  }
+};
