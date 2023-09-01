832861ae3589f45ba9caaa0d7407aba23793b167
mr-sazzad <main.sazza@gmail.com>
2023-09-01 23:56:45 +0600

    deleteSingleCategory is commited successfully

--- src/app/moduls/categories/category_controller.ts
+++ src/app/moduls/categories/category_controller.ts
@@ -69,3 +69,20 @@ export const updateSingleCategory: RequestHandler = async (req, res, next) => {
     next(err);
   }
 };
+
+export const deleteSingleCategory: RequestHandler = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await categoryServices.deleteSingleCategory(id);
+
+    res.status(200).json({
+      statusCode: 200,
+      success: true,
+      message: "Categories deleted Successfully 🔴",
+      data: result,
+    });
+  } catch (err: any) {
+    console.log(err);
+    next(err);
+  }
+};
