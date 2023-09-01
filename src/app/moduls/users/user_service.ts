95e3fa4777bcd2fda42bb0c268077296c03dd626
mr-sazzad <main.sazza@gmail.com>
2023-09-01 23:29:26 +0600

    login user service commited

--- src/app/moduls/users/user_service.ts
+++ src/app/moduls/users/user_service.ts
@@ -1,5 +1,8 @@
 import { User } from "@prisma/client";
+import jwt from "jsonwebtoken";
 import prisma from "../../utils/prismaDB";
+
+const secret = process.env.JWT_SECRET;
 
 const createSingleUser = async (data: User): Promise<User> => {
   const result = await prisma.user.create({
@@ -9,6 +12,32 @@ const createSingleUser = async (data: User): Promise<User> => {
   return result;
 };
 
+const loginUser = async (data: Partial<User>): Promise<string> => {
+  const email = data.email;
+
+  const isExist = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  if (!isExist) {
+    throw new Error("Not Authenticated");
+  }
+
+  const jwt_payload = {
+    id: isExist.id,
+    email: isExist.email,
+    role: isExist.role,
+    address: isExist.address,
+  };
+
+  const token = jwt.sign(jwt_payload, secret as string, { expiresIn: "365d" });
+
+  return token;
+};
+
 export const userServices = {
   createSingleUser,
+  loginUser,
 };
