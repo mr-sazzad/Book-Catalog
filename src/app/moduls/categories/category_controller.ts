54e9a5bd454d276bf34db0c73f9284920ddfe836
mr-sazzad <main.sazza@gmail.com>
2023-09-01 23:54:49 +0600

    getSingleCategory commited

--- src/app/moduls/categories/category_controller.ts
+++ src/app/moduls/categories/category_controller.ts
@@ -34,3 +34,20 @@ export const getAllCategories: RequestHandler = async (req, res, next) => {
     next(err);
   }
 };
+
+export const getSingleCategory: RequestHandler = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await categoryServices.getSingleCategory(id);
+
+    res.status(200).json({
+      statusCode: 200,
+      success: true,
+      message: "Categories retrieved Successfully 🦀",
+      data: result,
+    });
+  } catch (err: any) {
+    console.log(err);
+    next(err);
+  }
+};
