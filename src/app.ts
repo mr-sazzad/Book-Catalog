c7719d50bd3a7e5dbb872dd0e711c704d1215556
mr-sazzad <main.sazza@gmail.com>
2023-09-02 00:03:19 +0600

    app.ts file modified

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import cors from "cors";
-import express, { urlencoded } from "express";
+import express, { Request, Response, urlencoded } from "express";
 
 const app = express();
 
@@ -7,5 +7,20 @@ app.use(cors());
 app.use(express.json());
 
 app.use(urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome To This Project 🦀",
+    success: true,
+    author: "Sazzad Karim",
+    Date: "01/09/23",
+    name: "Assignment-8",
+    tools: "Prisma Express PostgreSQL Typescript",
+
+    routes: "base url -> http://localhost:5000/api/v1",
+    users: " base url -> http://localhost:5000/api/v1/users 🦀",
+    category: " base url -> http://localhost:5000/api/v1/categories ⚖️",
+  });
+});
 
 export default app;
